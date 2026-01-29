5803ace52ab481a1cb8d431aad2b2c86ecf88949
anass-lrj <anass.laaraj@viacesi.fr>
2026-01-29 09:54:58 +0100

    add folder for prosits

---
+++ Prosits_Csharp/Prosit_2/Design_Pattern/Strategie/Strategie.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prosits_Csharp.Prosit_2.Design_Pattern.Strategie
+{
+    internal class Strategie
+    {
+    }
+}
